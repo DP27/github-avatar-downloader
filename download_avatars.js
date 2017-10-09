8ef30b17a323181ed3184c30eb6314cd1b151234
Dishant <dishantpatel27@gmail.com>
2017-10-09 20:10:36 +0000

    created repo contributors function

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,17 @@
 var request = require('request'); // getting the request lib
 console.log('Welcome to the Github Avatar Downloader!');
 
+
+
+
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  return cb;
+};
+
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
