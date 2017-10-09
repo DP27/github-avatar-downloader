b7e9666065e36a2fdbdde4bad47840e10a9e76d0
Dishant <dishantpatel27@gmail.com>
2017-10-09 20:18:27 +0000

    created a request url

--- download_avatars.js
+++ download_avatars.js
@@ -1,11 +1,18 @@
 var request = require('request'); // getting the request lib
 console.log('Welcome to the Github Avatar Downloader!');
 
-
+const GITHUB_USER = "DP27";
+const GITHUB_TOKEN = "f75e68bad236949c50d9a4206ccd02e1f4996d24";
 
 
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
+
+
+
+
   return cb;
 };
 
