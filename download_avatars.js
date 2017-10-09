9ce8e0f8a3ae23a7fc4393f4cec7e07293bc5afd
Dishant <dishantpatel27@gmail.com>
2017-10-09 21:39:01 +0000

    Implemented get repo contributors function

--- download_avatars.js
+++ download_avatars.js
@@ -3,22 +3,37 @@ console.log('Welcome to the Github Avatar Downloader!');
 
 const GITHUB_USER = "DP27";
 const GITHUB_TOKEN = "f75e68bad236949c50d9a4206ccd02e1f4996d24";
-
+const UserAgent = "DP27";
 
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
 
+  //requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  var result = {};
+  const options = {
+    url: 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    method: 'GET',
+    headers: {
+        'User-Agent': 'DP27'
+    }
+  };
 
+  //console.log(requestURL);
+  request(options,function(err,result){
+    return cb(err,JSON.parse(result.body));
+  })
 
-
-  return cb;
 };
+
+
+
 
 
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
+  for(var i in result){
+    console.log(result[i]['avatar_url']);
+  }
 });
